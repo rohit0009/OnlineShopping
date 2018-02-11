b041f6062276fab3cde6c2b075f3f9bef54f6754
Rohit P Shewale <rohitpshewale@gmail.com>
2018-02-11 11:23:41 +0530

    First Commit

--- src/First.java
+++ src/First.java
@@ -1,4 +1,19 @@
+import java.io.IOException;
 
-public class First {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public class First extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
 }
